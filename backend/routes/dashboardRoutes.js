bc024463eb23a74cd43cba3bff829cd285d90f98
Ayush-7-z <ayush5sharma0456@gmail.com>
2026-03-15 10:38:20 +0530

    Delete backend directory

--- backend/routes/dashboardRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getDashboardStats } = require("../controllers/dashboardController");
-
-router.get("/", getDashboardStats);
-
-module.exports = router;
